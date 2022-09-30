85f757b2eef7ec1a418c77074918f3967e2a1ee3
qingge-yu <maggieqgg@gmail.com>
2022-09-29 17:33:11 -0700

    makes code less redundant

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const pageCtrl = require('../controllers/home')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'My Express Skills Page' })
 });
 
-router.get('/', pageCtrl.index)
+router.get('/', function index(req, res) {
+  res.redirect('/skills')
+})
 
 module.exports = router;
